55c79bdb27e5241749563b81c551ded6cccc1876
Aditya.Patel <aditya.patel@theonetechnologies.co.in>
2023-05-09 19:04:58 +0530

    search screen with formik

---
+++ src/Component/SearchComponent/SearchHeader.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+
+const SearchHeader = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.searchHeading}>What are you looking for ?</Text>
+    </View>
+  );
+};
+
+export default SearchHeader;
+
+const styles = StyleSheet.create({
+  searchHeading: {
+    fontSize: wp('10%'),
+    fontWeight: 'bold',
+    color: '#1C203D',
+  },
+});
--- src/Screens/SearchScreen.tsx
+++ src/Screens/SearchScreen.tsx
@@ -1,14 +1,22 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import SearchHeader from '../Component/SearchComponent/SearchHeader';
+import SearchForm from '../Component/SearchComponent/SearchForm';
 
 const SearchScreen = () => {
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={styles.container}>
+      <SearchHeader />
+      <SearchForm />
     </View>
   );
 };
 
 export default SearchScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
+});
