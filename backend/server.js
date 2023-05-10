a5270169bf5fba6360e0cc0d1fd3b8438a01288d
Aditya.Patel <aditya.patel@theonetechnologies.co.in>
2023-05-10 19:15:34 +0530

    api call failed

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
+const corsOption = {
+  credentials: true,
+  origin: ["http://localhost:8080"],
+};
 connectDb();
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(cors(corsOption));
 app.use(express.json());
 app.use("/api/BRT", require("./routes/routes"));
 app.use(errorHandler);
